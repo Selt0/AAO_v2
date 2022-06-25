91032b7484d269a8356d0e48bc73bf41b9c54761
Selt0 <chewedgum12@gmail.com>
2022-06-25 07:52:55 -0500

    Refactored and completed phase 2

--- 19_asynchronicity/02_guessing-game-project/guessingGame.js
+++ 19_asynchronicity/02_guessing-game-project/guessingGame.js
@@ -4,31 +4,75 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const secretNumber = 5
 
+const play = () => {
+    let min
+    let max
+    let secretNumber
 
-const checkGuess = (num) => {
-    if (num > secretNumber){
-        console.log('Too high.')
-        return false
-    } else if (num < secretNumber){
-        console.log('Too low.')
-        return false
-    } else {
-        console.log('Correct!')
-        return true
+    rl.question('Enter a max number: ', grabMaxNumber)
+
+    function grabMaxNumber(maxInput){
+        max = maxInput
+
+        rl.question('Enter a min number: ', grabMinNumber)
+    }
+
+    function grabMinNumber(minInput){
+        min = minInput
+
+        if(validInput(min, max)){
+            secretNumber = grabRandomNumber(min, max)
+
+            console.log(`I'm thinking of a number between ${min} and ${max}...`)
+
+            askGuess()
+        } else {
+            play()
+        }        
+    }
+
+    function askGuess() {
+        rl.question('Enter your guess: ', (answer) => {
+            if(checkGuess(Number(answer))){
+                console.log('You win!')
+                rl.close()
+            } else {
+                askGuess()
+            }
+        })
+    }
+
+    function checkGuess(num) {
+         if (num > secretNumber){
+            console.log('Too high.')
+            return false
+        } else if (num < secretNumber){
+            console.log('Too low.')
+            return false
+        } else {
+            console.log('Correct!')
+            return true
+        }
     }
 }
 
-const askGuess =  () => {
-    rl.question('Enter your guess: ', (answer) => {
-        if(checkGuess(Number(answer))){
-            console.log('You win!')
-            rl.close()
-        } else {
-            askGuess()
-        }
-    })
+play()
+
+
+
+// helpers
+function grabRandomNumber(min, max) {
+    min = Math.ceil(min); // round up
+    max = Math.floor(max); // round down
+    return Math.floor(Math.random() * (max - min + 1) + min); 
 }
 
-askGuess()
+function validInput(min,max){
+    if (max < min){
+        console.log('max must be greater than min')
+        return false
+    }
+
+    return true
+}
