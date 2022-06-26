a39359b6cb0f06c17697f83f07bf705da08bb728
Selt0 <chewedgum12@gmail.com>
2022-06-25 20:21:49 -0500

    complete inheritance

--- 21_object-oriented-programming/02_inheritance/classes/student.js
+++ 21_object-oriented-programming/02_inheritance/classes/student.js
@@ -1,6 +1,25 @@
 const Person = require('./person');
 
-// Your code here
+class Student extends Person{
+  constructor(firstName, lastName, major, GPA){
+    super(firstName, lastName)
+    this.major = major
+    this.GPA = GPA
+  }
+
+  static compareGPA(student1, student2){
+    let highestGPA
+    if (student1.GPA > student2.GPA){
+      highestGPA = student1
+    } else if (student2.GPA > student1.GPA){
+      highestGPA = student2
+    } else {
+       return 'Both students have the same GPA.'
+    }
+
+    return `${highestGPA.firstName} ${highestGPA.lastName} has the higher GPA.`
+  }
+} 
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
