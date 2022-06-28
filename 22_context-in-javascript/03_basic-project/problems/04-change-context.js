70e819c2652d21479316fee1b6f217e3d45a9f8e
Selt0 <chewedgum12@gmail.com>
2022-06-28 12:43:01 -0500

    complet phase 5

--- 22_context-in-javascript/03_basic-project/problems/04-change-context.js
+++ 22_context-in-javascript/03_basic-project/problems/04-change-context.js
@@ -2,17 +2,6 @@ function changeContext(func, obj) {
   return func.call(obj)
 }
 
-
-class Person {
-  constructor(name) {
-    this.name = name;
-  }
-}
-
-function extractName() {
-  return this.name;
-}
-
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
